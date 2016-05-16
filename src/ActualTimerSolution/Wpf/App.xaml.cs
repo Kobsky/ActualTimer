b195d0a58243badda24d91c6d75260d4c73e480c
Kobsky <kobelev.alexander@gmail.com>
2016-05-16 14:49:26 +0300

    AT_WPF-1 create wpf project and test

---
+++ src/ActualTimerSolution/Wpf/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Kobsky.ActualTimer.Wpf
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	// ReSharper disable once RedundantExtendsListEntry
+	public partial class App : Application
+	{
+	}
+}
